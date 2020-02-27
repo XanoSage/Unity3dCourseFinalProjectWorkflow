ad7d3b4ff60efdaeaf8d2c469c218fb059dbfc16
XanoSage <alexhram@gmail.com>
2020-02-27 17:23:51 +0200

    - added free useful assets; - added simple PoolManager;

---
+++ FinalProjectWorkflow/Assets/Scripts/Utils/PoolInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolInitializer : MonoBehaviour
+{
+    [SerializeField] private PoolInitializerData[] _poolInitializerDatas;
+
+    [SerializeField] private Transform _parentForPool;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PoolManager.Init(_parentForPool);
+
+        foreach (var initializerData in _poolInitializerDatas)
+        {
+            for (int i = 0; i < initializerData.Count; i++)
+            {
+                PoolManager.PutGameObjectToPool(initializerData.Prefab);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class PoolInitializerData
+{
+    public GameObject Prefab;
+    public int Count;
+}
---
+++ FinalProjectWorkflow/Assets/Scripts/Utils/PoolManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager 
+{
+    private static Dictionary<string, LinkedList<GameObject>> _poolDictionary;
+    private static Transform _poolParent;
+
+    public static void Init(Transform poolParent)
+    {
+        _poolParent = poolParent;
+        _poolDictionary = new Dictionary<string, LinkedList<GameObject>>();
+    }
+
+    public static GameObject GetGameObjectFromPool(GameObject prefab)
+    {
+        if (!_poolDictionary.ContainsKey(prefab.name))
+        {
+            _poolDictionary[prefab.name] = new LinkedList<GameObject>();
+        }
+
+        GameObject result = null;
+
+        if (_poolDictionary[prefab.name].Count > 0)
+        {
+            result = _poolDictionary[prefab.name].First.Value;
+            _poolDictionary[prefab.name].RemoveFirst();
+            result.SetActive(true);
+            return result;
+        }
+
+        result = GameObject.Instantiate(prefab);
+        result.name = prefab.name;
+
+        return result;
+    }
+
+    public static void PutGameObjectToPool(GameObject target)
+    {
+        _poolDictionary[target.name].AddFirst(target);
+        target.transform.SetParent(_poolParent);
+        target.SetActive(false);
+    }
+}
